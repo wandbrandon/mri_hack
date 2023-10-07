fcfc4d75838c401710ba4132c7989a43c3fbb557
Usdiez <austinrobertson1010@gmail.com>
2023-10-07 05:19:40 -0400

    navbar style edits

--- frontend/app/navbar.tsx
+++ frontend/app/navbar.tsx
@@ -1,4 +1,6 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
   const routes = [
@@ -12,10 +14,19 @@ const Navbar = () => {
     },
   ];
 
+  const currentPath = usePathname();
+  console.log(currentPath);
+
   return (
     <div className="absolute inset-x-1 mx-auto w-8/12 h-fit mt-14 py-2 rounded-full flex flex-row justify-evenly border-solid bg-mriSlate">
       {routes.map((route, index) => (
-        <Link className="hover:bg-mriBlue rounded-full px-16 transition ease-in text-lg" href={route.path} key={index}>
+        <Link
+          className={`${
+            currentPath === route.path ? "bg-mriBlue" : "hover:bg-mriBlue"
+          } rounded-full px-16 transition ease-in text-lg`}
+          href={route.path}
+          key={index}
+        >
           {route.title}
         </Link>
       ))}
