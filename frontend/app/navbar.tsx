e1d90bc7223bccdf8bf3f7a2ac0a07252d660314
Brandon Wand <wandbrandon@gmail.com>
2023-10-07 18:17:49 -0400

    navbar setup

--- frontend/app/navbar.tsx
+++ frontend/app/navbar.tsx
@@ -1,6 +1,8 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { motion } from "framer-motion";
+import { useRef, useState } from "react";
 
 const Navbar = () => {
   const routes = [
@@ -9,25 +11,39 @@ const Navbar = () => {
       title: "Home",
     },
     {
-      path: "/test",
+      path: "/",
       title: "Test",
+    },
+    {
+      path: "/",
+      title: "About",
+    },
+    {
+      path: "/",
+      title: "RUN",
     },
   ];
 
   const currentPath = usePathname();
   console.log(currentPath);
 
+  const [selectedId, setSelectedId] = useState<number | null>(null);
+
   return (
-    <div className="absolute inset-x-1 mx-auto w-8/12 h-fit mt-6 py-2 rounded-full flex flex-row justify-evenly border-solid bg-mriSlate">
+    <div className="absolute inset-x-1 mx-auto h-fit w-fit mt-6 py-3 px-3 rounded-full flex flex-row gap-6 justify-evenly border-solid bg-mriSlate z-0">
       {routes.map((route, index) => (
         <Link
-          className={`${
-            currentPath === route.path ? "bg-mriBlue" : "hover:bg-mriBlue"
-          } rounded-full px-16 transition ease-in text-lg`}
+          className="py-6 px-24 flex flex-row justify-center items-center rounded-full relative transition-all z-50"
+          onClick={() => setSelectedId(index)}
           href={route.path}
-          key={index}
         >
           {route.title}
+          {index === selectedId && (
+            <motion.div
+              className="absolute w-full h-full top-0 left-0 bg-mriBlue rounded-full z-10"
+              layoutId="outline"
+            ></motion.div>
+          )}
         </Link>
       ))}
     </div>
